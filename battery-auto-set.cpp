f6569ec46567fc088c06cfca2210aadbb009e537
SamuelPinaLopes <samuelpinalopesws@gmail.com>
2026-05-03 02:04:50 -0100

    I have the directory's name out of the function to start choosing the correct folder for each laptop

--- battery-auto-set.cpp
+++ battery-auto-set.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
 #include <filesystem>
+#include <vector>
+#include <iterator>
 
 
 using namespace std;
 
 
-void get_directories() {
-    filesystem::path way = "/sys/class/power_supply/";
+vector<filesystem::path> get_directories(filesystem::path defaultPath) { // default path to look
+    vector<filesystem::path> all; // array of all directories inside default path
 
-    filesystem::directory_iterator it(way);
-    filesystem::directory_iterator end;
+    filesystem::directory_iterator it(defaultPath); // starting the DIRECTORY_iterator and giving its start point
+    filesystem::directory_iterator end;// ending the DIRECTORY_iterator and it has nothing, end == '' so you don't have to pass nothing.
 
-    for (; it != end; ++it) {
-        filesystem::directory_entry entry = *it;
+    for (; it != end; ++it) { // loop through the directory_iterator
+        filesystem::directory_entry entry = *it; // entry is like a variable that recives the value that it is pointing
 
-        if (entry.is_directory()) {
-            cout << "Directories: " << entry.path().filename() << endl;
+        if (entry.is_directory()) { // if is really a directory
+            all.push_back(entry.path().filename()); // adding all directories inside array to return
         }
     }
+
+    return all;
 }
+
+
+string power_supply_info("/sys/class/power_supply/");
+
 
 int main() {
 
-    get_directories();
+    vector<filesystem::path> directories(get_directories(power_supply_info)); // creating a vector with return of directories function
+
+    vector<filesystem::path>::iterator it; // iterator for indexing elements of directories array
+
+    for (it = directories.begin(); it != directories.end(); ++it) { // loop through
+        cout << "directory: " << *it << endl;
+    }
 
     return 0;
 }
