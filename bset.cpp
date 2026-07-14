1ee517c2c1ef8db48fad63fc0046394518d5a9f9
SamuelPinaLopes <samuelpinalopesws@gmail.com>
2026-07-14 13:35:56 -0100

    testing c-style strings

--- bset.cpp
+++ bset.cpp
@@ -6,34 +6,9 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
     for (int index = 0; index < argc; index++) {
-        cout << argv[index] << "    " << endl;
-        
-        if (argv[index] == "-u" or argv[index] == "--update") {
-            apply_updates();
-        
-        } else if (argv[index] == "-su" or argv[index] == "--search-update") {
-            search_updates();
-
-        } else if (argv[index] == "-s" or argv[index] == "--show-settings") {
-            show_settings();
-
-        } else if (argv[index] == "-ls" or argv[index] == "--load-settings") {
-            load_settings();
-
-        } else if (argv[index] == "-sp" or argv[index] == "--save-preset") {
-            save_settings_preset();
-
-        } else if (argv[index] == "-dflt" or argv[index] == "--default-settings") {
-            set_default_settings();
-
-        } else if (argv[index] == "-gen" or argv[index] == "--gen-default-config") {
-            gen_default_config();
+        if () {
             
-        } else if (argv[index] == "-p" or argv[index] == "--show-paths") {
-            show_paths();
-
         }
-
     }
     
     return 0;
