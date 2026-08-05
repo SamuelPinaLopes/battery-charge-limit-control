9688308407477b47a540493f8571878b1200e205
SamuelPinaLopes <samuelpinalopesws@gmail.com>
2026-08-05 08:52:38 -0100

    c-style strings tests finished

--- bset.cpp
+++ bset.cpp
@@ -5,10 +5,21 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-    for (int index = 0; index < argc; index++) {
-        if () {
-            
+    for (int indexarray = 0; indexarray < argc; indexarray++) {
+        // word to check
+        char check[] = "--help";
+        // first char inside array slot
+        int indexchar = 0;
+        //     array slot index|index of char value
+        while (argv[indexarray][indexchar] != '\0') {
+            cout << "index: " << indexchar << "  character: " << argv[indexarray][indexchar] << endl;
+            // incrementing
+            indexchar++;
+
         }
+
+        cout << "\n\n" << endl;
+    
     }
     
     return 0;
