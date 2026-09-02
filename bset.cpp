3440473eaaae661383a6cc36d6d84a9fdaa424ca
SamuelPinaLopes <samuelpinalopesws@gmail.com>
2026-09-02 05:48:17 -0100

    optmizing bset parameters code

--- bset.cpp
+++ bset.cpp
@@ -1,70 +1,66 @@
 #include <iostream>
 #include <ostream>
 #include <string>
-#include <cstring>
+// #include <cstring>
 
 #include "source/battery_set_functions.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // variables
-    int index_array_parameter = 1;
+    // get each parameter
+    //                        general help
+    if ( // if you put one parameters name, you'll have to put for all
+        check_words(argv[1], "--help") == true or
+        check_words(argv[1], "-h", true) == true
+    ) {
+        // shows the help
+        cout << "yes it worked :O\n for general help" << endl;
+    }
 
-    // get each word/parameter
-    while (argv[index_array_parameter] != NULL) {
-        //                        general help
-        if ( // if you put one parameters name, you'll have to put for all
-            check_words(argv[index_array_parameter], "--help") == true or
-            check_words(argv[index_array_parameter], "-h", true) == true
-        ) {
-            // shows the help
-            cout << "yes it worked :O\n for general help" << endl;
+    //                      program's settings
+    // show current settings
+    if (
+        check_words(argv[1], "--show-settings") or
+        check_words(argv[1], "-cs", true)
+    ) {
+        if (show_settings() == false) {
+            cout << "\nSomething went wrong showing user settings..." << endl;
         }
+    }
 
-        //                      program's settings
-        // show current settings
-        if (
-            check_words(argv[index_array_parameter], "--show-settings") or
-            check_words(argv[index_array_parameter], "-cs", true)
-        ) {
-            if (show_settings() == false) {
-                cout << "\nSomething went wrong showing user settings..." << endl;
-            }
+    // load settings from file
+    if (
+        check_words(argv[1], "--load-settings") or
+        check_words(argv[1], "-ls")
+    ) {
+        if (load_settings(argc, argv) == false) {
+            cout << "\nSomething went wrong loading user custom settings from file..." << endl;
         }
+    }
 
-        // load settings from file
-        if (
-            check_words(argv[index_array_parameter], "--load-settings") or
-            check_words(argv[index_array_parameter], "-ls")
-        ) {
-            load_settings();
-        }
-        // save settings as preset / on a file
-        if (
-            check_words(argv[index_array_parameter], "--save-preset") or
-            check_words(argv[index_array_parameter], "-sp")
-        ) {
-            save_settings_preset();
-        }
-        // use default settings
-        if (
-            check_words(argv[index_array_parameter], "--default") or
-            check_words(argv[index_array_parameter], "-d")
-        ) {
-            set_default_settings();
-        }
-        // show all paths in use
-        if (
-            check_words(argv[index_array_parameter], "--show-paths") or
-            check_words(argv[index_array_parameter], "-SP")
-        ) {
-            show_paths();
-        }
+    // save settings as preset on file
+    if (
+        check_words(argv[1], "--save-settings-preset") or
+        check_words(argv[1], "-sp")
+    ) {
+        save_settings_preset();
+    }
 
-        // incrementing argv index
-        index_array_parameter++;
+    // use default settings
+    if (
+        check_words(argv[1], "--default") or
+        check_words(argv[1], "-d")
+    ) {
+        set_default_settings();
+    }
 
+    // show all paths in use
+    if (
+        check_words(argv[1], "--show-paths") or
+        check_words(argv[1], "-SP")
+    ) {
+        show_paths();
     }
 
     return 0;
